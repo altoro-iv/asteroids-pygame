b1dd011e7e6666554730f970e00ea7685df57839
Miguel Altoro <altoro.iv@gmail.com>
2025-07-18 23:32:31 -0500

    Add time clock object to limit frames per second to 60

--- main.py
+++ main.py
@@ -8,21 +8,30 @@ from constants import *
 def main():
     # initialize pygame
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # create a clock object
+    clock = pygame.time.Clock()
+    dt = 0
 
-    # creat game loop
-    counter = 0
-    while counter == 0:
+    # create game loop
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        # fills the whole screen with color black
         pygame.Surface.fill(screen, "black")
         pygame.display.flip()
+
+        # set fps to 60 frames per second
+        clock.tick(60)
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
