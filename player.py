74acf9fcb2999a46731e216a14be533e4ea5379d
Miguel Altoro <altoro.iv@gmail.com>
2025-07-21 18:19:38 -0500

    Add move function so player object can move forward and backward.

--- player.py
+++ player.py
@@ -25,7 +25,16 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
 
     # method for player class to rotate
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+
+    # method for player class to move back and forth with the W and S keys
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
